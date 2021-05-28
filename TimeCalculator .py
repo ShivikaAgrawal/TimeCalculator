ca1d05a9acc0476ad4aa7bc0135d0939ccf1df17
ShivikaAgrawal <74872250+ShivikaAgrawal@users.noreply.github.com>
2021-05-28 22:05:09 +0530

    Updation on 28May,2020

--- TimeCalculator .py
+++ TimeCalculator .py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
-
-
 import pytz, datetime  ##importing libraries
 year = int(input())
 month = int(input())
@@ -11,35 +8,14 @@ day = int(input())
 hours = int(input())
 minutes= int(input())
 
-
-# In[10]:
-
-
 #converting into timestamp
-
-
-# In[20]:
-
-
-curr_date=datetime.datetime(year,month,day,hours,minutes)
-
-
-# In[ ]:
-
+curr_date=datetime.datetime(year,month,day,hours,minutes) #converting into timestamp
 
 #To get time of that city
-
-
-# In[16]:
-
-
-cairo_tz=pytz.timezone('Africa/cairo')
+cairo_tz=pytz.timezone('Africa/cairo') #To get time of that city
 london_tz=pytz.timezone('utc')
 Newdelhi_tz=pytz.timezone('Asia/kolkata')
 Sydney_tz=pytz.timezone('Australia/sydney') 
-
-
-# In[17]:
 
 
 cairo_time=pytz.utc.localize(curr_date).astimezone(cairo_tz) 
@@ -47,23 +23,12 @@ london_time=pytz.utc.localize(curr_date).astimezone(london_tz)
 Newdelhi_time=pytz.utc.localize(curr_date).astimezone(Newdelhi_tz)
 Sydney_time=pytz.utc.localize(curr_date).astimezone(Sydney_tz)
 
-
-# In[12]:
-
-
 #print Timestamp of particular city in ISO format
-
-
-# In[21]:
-
 
 print("Cairo Time is",cairo_time.isoformat())  
 print("London Time is",london_time.isoformat())
 print("New Delhi Time is",Newdelhi_time.isoformat())
 print("Sydney Time is",Sydney_time.isoformat())
-
-
-# In[ ]:
 
 
 
